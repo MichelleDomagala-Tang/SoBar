a37a0cf64d856bf7d9e816f3db5ba59ccff1edcb
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2020-04-08 17:18:29 -0400

    Updated modal for preferences

--- Frontend/soBarApp/App.js
+++ Frontend/soBarApp/App.js
@@ -4,22 +4,14 @@ import { globalStyles } from './styles/global';
 
 import Home from './screens/home'
 import Map from './screens/map'
-import Preferences from './screens/preferences'
 
 export default function App() {
-  const [pref, setpref] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
 
   let content = <Home onStart={changeMap} />;
 
   const changeMap = () => {
     content = <Map />;
   };
-
-  const selectPref = prefTitle => {
-    setpref(currentPref => [...pref, { key: Math.random().toString(), value: prefTitle }])
-    setModalVisible(false);
-  }
 
   return (
     <View>
@@ -29,12 +21,6 @@ export default function App() {
       <View>
         {content}
       </View>
-      <View style={globalStyles.buttonContainer} >
-        <Button title="Preferences Button" style={globalStyles.button} onPress={() => setModalVisible(true)}>PREFERENCES</Button>
-        <Button title="Find Route" style={globalStyles.button} onPress={() => props.onStart}>FIND ROUTE</Button>
-        <Preferences visible={modalVisible} />
-      </View>
-      {content}
     </View>
   )
 }
--- Frontend/soBarApp/screens/home.js
+++ Frontend/soBarApp/screens/home.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { View, Text, StyleSheet, Button, Image, Transformation } from 'react-native';
 import { globalStyles } from '../styles/global';
 
+import Preferences from './screens/preferences'
+
 const HomeScreen = props => {
+
+    const [pref, setpref] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const selectPref = prefTitle => {
+        setpref(currentPref => [...pref, { key: Math.random().toString(), value: prefTitle }])
+        setModalVisible(false);
+    }
+
     return (
-    <View>
-        <View style={globalStyles.buttonContainer} >
-            <Button title="Map" onPress={() => props.onStart} color='grey'/>
-            <Button title="Map Screen" onPress={() => props.onStart} color='grey' />
+        <View>
+            <View style={globalStyles.buttonContainer} >
+                <Button title="Map" onPress={() => props.onStart} color='grey' />
+                <Button title="Map Screen" onPress={() => props.onStart} color='grey' />
+            </View>
+            <View style={globalStyles.buttonContainer} >
+                <Button title="Preferences Button" style={globalStyles.button} onPress={() => setModalVisible(true)}>PREFERENCES</Button>
+                <Button title="Find Route" style={globalStyles.button} onPress={() => props.onStart}>FIND ROUTE</Button>
+                <Preferences visible={modalVisible} />
+            </View>
         </View>
-    </View>
     );
 };
 
