c748b4b7e125ba09014895d9aeca021956d6a442
Michelle Domagala-Tang <45515385+domagalm@users.noreply.github.com>
2020-04-09 14:43:33 -0400

    Updated map

--- Frontend/soBarApp/screens/map.js
+++ Frontend/soBarApp/screens/map.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import { globalStyles } from '../styles/global';
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -9,34 +10,36 @@ const instructions = Platform.select({
 
 export default function MapScreen() {
   return (
+    <View>
+      <Button title="Home Page" styles={{ position: 'absolute' }} />
+    </View>
     <View style={styles.container}>
 
       <MapView style={styles.map}
-          region={{
+        region={{
+          latitude: 43.2600052,
+          longitude: -79.9277422,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }} >
+        <MapView.Marker
+          coordinate={{
             latitude: 43.2600052,
-            longitude: -79.9277422,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1
-          }} >
-            <MapView.Marker
-              coordinate={{
-                latitude: 43.2600052,
-                longitude: -79.9277422
-              }}
-              title={'free samples'}
-              description={'jk its rona szn'}
-              />
+            longitude: -79.9277422
+          }}
+          title={'free samples'}
+          description={'jk its rona szn'}
+        />
 
-              
 
-              <MapView.Marker
-              coordinate={{
-                latitude: 42.2600052,
-                longitude: -78.9277422
-              }}
-              title={'Michelles feet'}
-              description={'jk its rona szn'}
-              />
+        <MapView.Marker
+          coordinate={{
+            latitude: 42.2600052,
+            longitude: -78.9277422
+          }}
+          title={'Michelles feet'}
+          description={'jk its rona szn'}
+        />
 
       </MapView>
 
@@ -46,24 +49,22 @@ export default function MapScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    position: 'absolute',
-    top: 0,
+    top: 200,
     left: 0,
     bottom: 0,
     right: 0,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  map : {
-    position: 'absolute',
-    height: 200,
-    width: 300,
-    top: 0,
+  map: {
+    height: 500,
+    width: 400,
+    top: 200,
     left: 0,
     bottom: 0,
     right: 0,
 
   }
-  
+
 
 });
