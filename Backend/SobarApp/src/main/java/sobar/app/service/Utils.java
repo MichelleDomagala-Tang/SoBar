899ae49fb186960c0a8264e87bba2cf593d24604
Waseef Nayeem <haxerus.corporation@gmail.com>
2020-04-09 20:20:17 -0400

    Path and filter query endpoints.

--- Backend/SobarApp/src/main/java/sobar/app/service/Utils.java
+++ Backend/SobarApp/src/main/java/sobar/app/service/Utils.java
@@ -1,5 +1,7 @@
 package sobar.app.service;
 
+import sobar.app.service.model.Business;
+
 public class Utils {
     public static double dist(double lat1, double lon1, double lat2, double lon2) {
         double phi1 = Math.toRadians(lat1), phi2 = Math.toRadians(lat2), delta = Math.toRadians(lon2-lon1), R = 6371e3; // gives d in metres
--- Backend/SobarApp/src/main/java/sobar/app/service/controller/BusinessController.java
+++ Backend/SobarApp/src/main/java/sobar/app/service/controller/BusinessController.java
@@ -1,8 +1,11 @@
 package sobar.app.service.controller;
 
+import javafx.util.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import sobar.app.service.Utils;
+import sobar.app.service.algorithm.Graph;
+import sobar.app.service.algorithm.ShortestHamiltonianPath;
 import sobar.app.service.exception.BusinessNotFoundException;
 import sobar.app.service.exception.InvalidParamFormatException;
 import sobar.app.service.model.Business;
@@ -55,6 +58,75 @@ public class BusinessController {
         }
 
         return nearby;
+    }
+
+    @GetMapping("/nearbyfiltered")
+    public List<Business> getNearbyWithPrefs(@RequestParam String origin, @RequestParam Integer radius, @RequestParam List<String> pref) {
+        String[] tok = origin.split(",");
+
+        if (tok.length != 2) {
+            throw new InvalidParamFormatException();
+        }
+
+        List<Business> nearby = new ArrayList<>();
+        double lat = Double.parseDouble(tok[0]);
+        double lon = Double.parseDouble(tok[1]);
+
+        for (Business b : repository.findAll()) {
+            if (Utils.dist(lat, lon, b.getLatitude(), b.getLongitude()) <= radius) {
+                for (String s : pref) {
+                    if (b.getCategories().toLowerCase().contains(s.toLowerCase())) {
+                        nearby.add(b);
+                    }
+                }
+            }
+        }
+
+        return nearby;
+    }
+
+    @GetMapping("/path")
+    public List<Long> getPath(@RequestParam String origin, @RequestParam final List<Long> id) {
+        String[] tok = origin.split(",");
+
+        if (tok.length != 2) {
+            throw new InvalidParamFormatException();
+        }
+
+        double lat = Double.parseDouble(tok[0]);
+        double lon = Double.parseDouble(tok[1]);
+
+        List<Business> businesses = repository.findAllById(id);
+        Business start = businesses.get(0);
+
+        Graph g = new Graph();
+
+        for (Business b : businesses) {
+            g.addVertex(b);
+        }
+
+        for (Business b : businesses) {
+            double currentDist =  Utils.dist(lat, lon, b.getLatitude(), b.getLongitude());
+            double minDist = Utils.dist(lat, lon, start.getLatitude(), start.getLongitude());
+            if (currentDist < minDist) {
+                start = b;
+            }
+
+            for (Business bb : businesses) {
+                if (!bb.equals(b)) {
+                    g.addEdge(b, bb, Utils.dist(b.getLatitude(), b.getLongitude(), bb.getLatitude(), bb.getLongitude()));
+                }
+            }
+        }
+
+        ShortestHamiltonianPath shp = new ShortestHamiltonianPath(g, start);
+
+        List<Long> path = new ArrayList<>();
+        for (Business b : shp.findShortest()) {
+            path.add(b.getId());
+        }
+
+        return path;
     }
 
     @GetMapping("/businesses/{id}")
